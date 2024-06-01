3fd81242582372c75620a955e68a71a8a14cb919
Huy T Tran <tunhuytrn@gmail.com>
2024-05-31 23:03:16 -0400

    add review and sentiment analyzer functionalities

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -25,9 +25,20 @@ def get_request(endpoint, **kwargs):
     except:
         print("Network exception occured")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        res = requests.get(request_url)
+        return res.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 
-# def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
