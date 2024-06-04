d62a9a6cdf0d069ded3c180442ee7e5f0d5074c7
Huy T Tran <tunhuytrn@gmail.com>
2024-06-04 16:53:31 -0400

    check lint in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,7 +13,7 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs ):
+    if( kwargs ): 
         for key, value in kwargs.items():
             params = params + key+'='+value+"&"
 
@@ -24,6 +24,7 @@ def get_request(endpoint, **kwargs):
         response = requests.get(request_url)
         return response.json()
     except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
         print("Network exception occured")
 
 
@@ -40,8 +41,9 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
         print("Network exception occurred")
