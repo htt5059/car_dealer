f5fe440433e9d06c6b0c058e910fb3682608c1aa
Huy T Tran <tunhuytrn@gmail.com>
2024-06-05 14:03:14 -0400

    check lint in populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -14,7 +14,7 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(
         CarMake.objects.create(
-            name=data['name'], 
+            name=data['name'],
             description=data['description']
         )
     )
@@ -57,5 +57,5 @@ def initiate():
     for data in car_model_data:
         CarModel.objects.create(
             name=data['name'],
-            car_make=data['car_make'], 
+            car_make=data['car_make'],
             type=data['type'], year=data['year'])
